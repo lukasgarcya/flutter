e07f9ec3b5f4018ca26f8c778cfcd8a66f7d028f
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2020-03-04 16:07:09 -0300

    Usa um entrypoint para carregar xvfb, x11vnc e emulador

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Note: I've written this using sh so it works in the busybox container too
+
+# USE the trap if you need to also do manual cleanup after the service is stopped,
+#     or need to start multiple services in the one container
+trap "echo TRAPed signal" HUP INT QUIT TERM
+
+# start service in background here
+#$ANDROID_HOME/emulator/emulator -use-system-libs @teste -no-boot-anim -no-audio -no-window -writable-system -memory 2048
+Xvfb :1 -screen 0 1280x720x24+32 &
+#sleep 5
+x11vnc -display :1 &
+export DISPLAY=:1
+$ANDROID_HOME/emulator/emulator -use-system-libs @teste -no-boot-anim -no-audio -writable-system -memory 2048
+
+
+echo "[hit enter key to exit] or run 'docker stop <container>'"
+read
+
+# stop service and clean up here
+echo "stopping android emulator"
+adb -s emulator-5554 emu kill
+
+
+echo "exited $0"
