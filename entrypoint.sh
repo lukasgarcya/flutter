7a0277452586c53eec67d852851e4810a7abf2f9
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2020-03-04 23:37:00 -0300

    Usa usuário root para permitir acesso ao kvm. O emulador é executado com usuário android

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,19 @@ trap "echo TRAPed signal" HUP INT QUIT TERM
 
 # start service in background here
 #$ANDROID_HOME/emulator/emulator -use-system-libs @teste -no-boot-anim -no-audio -no-window -writable-system -memory 2048
+chown root:kvm /dev/kvm
+chmod g+rw /dev/kvm
 Xvfb :1 -screen 0 1280x720x24+32 &
 #sleep 5
 x11vnc -display :1 &
 export DISPLAY=:1
-$ANDROID_HOME/emulator/emulator -use-system-libs @teste -no-boot-anim -no-audio -writable-system -memory 2048
-
+su android -c "$ANDROID_HOME/emulator/emulator -use-system-libs @teste -no-boot-anim -no-audio -writable-system -memory 2048"
 
 echo "[hit enter key to exit] or run 'docker stop <container>'"
 read
 
 # stop service and clean up here
-echo "stopping android emulator"
+echo "stopping android emulator, Xvfb and x11vnc"
 adb -s emulator-5554 emu kill
-
 
 echo "exited $0"
